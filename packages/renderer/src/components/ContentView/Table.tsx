5d871ab87b0e1812d6c1b51c1db7686eb34ee4e8
Vu Nguyen <aqaurius6666@gmail.com>
2023-05-10 11:24:06 +0700

    fix: scroll

--- packages/renderer/src/components/ContentView/Table.tsx
+++ packages/renderer/src/components/ContentView/Table.tsx
@@ -14,13 +14,15 @@ const cellWidth = 14;
 export const MatrixTable = memo((props: MatrixTableProps) => {
   const { phylipMatrix, onCellClick, highlightCell } = props;
   const maxColumns = phylipMatrix.dimension.columns;
-  const [matrixToRender, setMatrixToRender] = useState<PhylipMatrix & { currentRight: number }>({
+  const [matrixToRender, setMatrixToRender] = useState<PhylipMatrix & {currentRight: number}>({
     ...phylipMatrix,
     matrix: phylipMatrix.matrix.map(e => e.slice(0, initialSize)),
     majors: phylipMatrix.majors.slice(0, initialSize),
     currentRight: initialSize,
   });
   const onScrollHandlerTimeoutRef = useRef<NodeJS.Timeout>();
+  const lastHighlightCellRef = useRef<Element>();
+  // const currentRightRef = useRef<number>(initialSize);
 
   const onScroll = useCallback((e: any) => {
     if (!e.currentTarget) return;
@@ -34,29 +36,28 @@ export const MatrixTable = memo((props: MatrixTableProps) => {
   }, []);
 
   const loadToRightIndex = (rightIndex: number) => {
-    if (!matrixToRender) return;
-    if (rightIndex < matrixToRender.currentRight) return;
-
-    phylipMatrix.majors.slice(matrixToRender.currentRight, rightIndex);
+    if (rightIndex <= matrixToRender.currentRight) return;
 
     const newMajors = matrixToRender.majors.concat(
       ...phylipMatrix.majors.slice(matrixToRender.currentRight, rightIndex),
     );
     const newMatrix = matrixToRender.matrix.map((line, index) => {
       return line.concat(
-        phylipMatrix.matrix[index].slice(matrixToRender!.currentRight, rightIndex),
+        phylipMatrix.matrix[index].slice(matrixToRender.currentRight, rightIndex),
       );
     });
-
-    setMatrixToRender(e => ({
-      ...e,
-      matrix: newMatrix,
-      majors: newMajors,
-      currentRight: rightIndex,
-    }));
+    
+    setMatrixToRender(e => {
+      if (e.currentRight >= rightIndex) return e;
+      return {
+        ...e,
+        matrix: newMatrix,
+        majors: newMajors,
+        currentRight: rightIndex,
+      };
+    })
   };
   const onScrollHandler = (currentTarget: any) => {
-    if (!matrixToRender) return;
     const { scrollLeft, offsetWidth } = currentTarget;
 
     const rightIndex = Math.min(Math.floor((scrollLeft + offsetWidth * 4) / cellWidth), maxColumns);
@@ -65,43 +66,27 @@ export const MatrixTable = memo((props: MatrixTableProps) => {
 
   useEffect(() => {
     if (!highlightCell) return;
-    if (!matrixToRender) return;
     const { col, row } = highlightCell;
     if (col >= matrixToRender.currentRight) {
-      loadToRightIndex(col + 1);
+      loadToRightIndex(col + 2);
     }
     setTimeout(() => {
       const cell = document.querySelector(`[data-col="${col}"][data-row="${row}"]`);
-      if (cell) {
-        cell.scrollIntoView({ behavior: 'smooth', block: 'nearest', inline: 'center' });
-      }
+      if (!cell) return;
+      if (lastHighlightCellRef.current) lastHighlightCellRef.current.classList.remove('highlighted');
+      cell.classList.add('highlighted');
+      lastHighlightCellRef.current = cell;
+      cell.scrollIntoView({ behavior: 'smooth', block: 'nearest' });
     }, 0);
   }, [highlightCell]);
+
 
   if (!matrixToRender) return <></>;
 
   return (
     <>
       <div className="phylip-content-view unselectable">
-        <div style={{ height: 'inherit' }}>
-          <table className="name-table">
-            <tbody>
-              <tr>
-                <td>Names</td>
-              </tr>
-              {matrixToRender.names.map((name, index) => (
-                <tr key={`names-${index}`}>
-                  <td
-                    key={`names-${index}`}
-                    className="cell cell-on-screen"
-                  >
-                    {name}
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
+        <NameTables names={matrixToRender.names} />
         <Scrollbars
           onScroll={onScroll}
           className="scrollbars"
@@ -111,45 +96,90 @@ export const MatrixTable = memo((props: MatrixTableProps) => {
             onClick={onCellClick}
           >
             <tbody>
-              <tr>
-                {matrixToRender.majors.map((major, index) => (
-                  <td
-                    key={`major-${index}`}
-                    className={`${major} cell cell-on-screen`}
-                  >
-                    {major}
-                  </td>
-                ))}
-              </tr>
-              {matrixToRender.matrix.map((line, row) => (
-                <tr key={`line-${row}-len-${line.length}`}>
-                  {line.split('').map((char, col) => {
-                    const isHightlighted =
-                      highlightCell && highlightCell.col === col && highlightCell.row === row;
-                    let className = isHightlighted
-                      ? 'cell cell-on-screen highlighted'
-                      : 'cell cell-on-screen';
-                    className += char === '.' ? ` ${matrixToRender!.majors[col]} dot` : ` ${char}`;
-                    const key = `line-${row}-${col}${isHightlighted ? '-highlighted' : ''}`;
-                    return (
-                      <td
-                        key={key}
-                        data-row={row}
-                        data-col={col}
-                        className={className}
-                      >
-                        {char}
-                      </td>
-                    );
-                  })}
-                </tr>
-              ))}
+              <MajorRows majors={matrixToRender.majors} currentRight={matrixToRender.currentRight}/>
+              <MatrixRows matrix={matrixToRender.matrix} majors={matrixToRender.majors} currentRight={matrixToRender.currentRight} />
             </tbody>
           </table>
         </Scrollbars>
       </div>
     </>
   );
+});
+
+type NameTablesProps = {
+  names: string[];
+}
+
+const MajorRows = memo(({ majors }: { majors: PhylipMatrix['majors'], currentRight: number }) => {
+  return (
+    <tr>
+      {majors.map((major, index) => (
+        <td
+          key={`major-${index}`}
+          className={`${major} cell cell-on-screen`}
+        >
+          {major}
+        </td>
+      ))}
+    </tr>
+  );
+}, (prevProps, nextProps) => {
+  console.log("majors", prevProps.currentRight, nextProps.currentRight)
+  return prevProps.currentRight === nextProps.currentRight;
+});
+
+const MatrixRows = memo(({ matrix, majors, currentRight }: { matrix: PhylipMatrix['matrix'], majors: PhylipMatrix['majors'], currentRight: number }) => {
+  return (
+    <>
+      {matrix.map((line, row) => (
+        <tr key={`line-${row}-len-${line.length}`}>
+          {line.split('').map((char, col) => {
+            let className = 'cell cell-on-screen';
+            className += char === '.' ? ` ${majors[col]} dot` : ` ${char}`;
+            const key = `line-${row}-${col}`;
+            return (
+              <td
+                key={key}
+                data-row={row}
+                data-col={col}
+                className={className}
+              >
+                {char}
+              </td>
+            );
+          })}
+        </tr>
+      ))}
+    </>
+  )
+}, (prevProps, nextProps) => {
+  return prevProps.currentRight === nextProps.currentRight;
+});
+
+export const NameTables = memo((props: NameTablesProps) => {
+  const { names } = props;
+  return (
+    <div>
+      <table className="name-table">
+        <tbody>
+          <tr>
+            <td>Names</td>
+          </tr>
+          {names.map((name, index) => (
+            <tr key={`names-${index}`}>
+              <td
+                className="cell cell-on-screen"
+              >
+                {name}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  )
+}, (prevProps, nextProps) => {
+  return JSON.stringify(prevProps) === JSON.stringify(nextProps);
 });
 
 export const CellPosition = (props: any) => {
@@ -159,16 +189,16 @@ export const CellPosition = (props: any) => {
 
   useEffect(() => {
     if (formRef.current) {
-      (formRef.current!.elements[0] as any).value = row;
-      (formRef.current!.elements[1] as any).value = col;
+      (formRef.current!.elements[0] as any).value = row + 1;
+      (formRef.current!.elements[1] as any).value = col + 1;
     }
   }, [row, col]);
 
   const onKeyDown = (e: any) => {
     if (e.key === 'Enter') {
       e.preventDefault();
-      const row = parseInt((formRef.current!.elements[0] as any).value);
-      const col = parseInt((formRef.current!.elements[1] as any).value);
+      const row = parseInt((formRef.current!.elements[0] as any).value) - 1;
+      const col = parseInt((formRef.current!.elements[1] as any).value) - 1;
       if (row >= 0 && row < maxRows && col >= 0 && col < maxColumns) {
         onCellSubmit({ row, col });
       }
